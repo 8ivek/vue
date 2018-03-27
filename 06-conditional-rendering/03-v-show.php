115482036dfdd6299105688839e18bf8ef04fea6
Bivek Joshi @ Home <bivek_j@yahoo.com>
2018-03-27 00:36:06 -0400

    Before List Rendering

---
+++ 06-conditional-rendering/03-v-show.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	<title>V-show</title>
+	<script src="../js/vue.js"></script>
+</head>
+<body>
+<h2>V-show</h2>
+<div id="app">
+	<button v-on:click="apple">Toggle</button>
+	<h1 v-show="ok">Hello!</h1>
+</div>
+<script>
+	var vm = new Vue({
+		el: '#app',
+		data: {
+			ok: true,
+		},
+		methods:{
+			apple: function(){
+				this.ok = (this.ok === true)?false:true;
+			}
+		},
+	});
+</script>
+</body>
+</html>
