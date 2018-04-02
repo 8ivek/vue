7d0c4a8a8a46dff47812ed95e6992856d1d12569
Bivek Joshi @ Home <bivek_j@yahoo.com>
2018-04-01 23:23:42 -0400

    favicons added.

--- 01-Essentials/03-template-syntax/03-attributes.php
+++ 01-Essentials/03-template-syntax/03-attributes.php
@@ -1,6 +1,9 @@
 <html>
 <head>
 	<title>Attributes</title>
+    <link rel="icon" type="image/png" sizes="32x32" href="../../favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="../../favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../../favicon/favicon-16x16.png">
 	<script src="../../js/vue.js"></script>
 </head>
 <body>
